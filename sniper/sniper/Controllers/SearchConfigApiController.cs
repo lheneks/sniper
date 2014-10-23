bbbe777c07494d6acfe9dbbf67cede9182320744
lheneks <lheneks@gmail.com>
2014-10-23 08:59:48 -0700

    add deleting configs backend, fix json content too long

--- sniper/sniper/Controllers/SearchConfigApiController.cs
+++ sniper/sniper/Controllers/SearchConfigApiController.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using Raven.Client;
 using sniper.Common.Mapping;
 using sniper.Models;
@@ -41,16 +43,34 @@ namespace sniper.Controllers
             return Json(new {Message = "Success"});
         }
 
-        public JsonResult List()
+        public ContentResult List()
         {
             var searchConfigs = RavenSession.Query<SearchConfig>().ToList();
+            var serializer = new JavaScriptSerializer {MaxJsonLength = Int32.MaxValue};
+            var result = new ContentResult
+            {
+                Content = serializer.Serialize(searchConfigs),
+                ContentType = "application/json"
+            };
 
-            return Json(searchConfigs);
+            return result;
         }
 
         public void SaveRunData(SearchData data)
         {
             RavenSession.Store(data);
+        }
+
+        public void DeleteConfig(int configId)
+        {
+            var datas = RavenSession.Query<SearchData>().Where(sd => sd.SearchConfigId == configId).ToList();
+            foreach (var searchData in datas)
+            {
+                RavenSession.Delete(searchData);
+            }
+
+            var config = RavenSession.Load<SearchConfig>(configId);
+            RavenSession.Delete(config);
         }
     }
 }
